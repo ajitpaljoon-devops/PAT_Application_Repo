0e19e766c7ddfe0eca342b3017520bfa31285f0b
AJIT PAL - Cloud & DevOps Engineer <ajitpal.joon@gmail.com>
2026-09-02 22:40:37 +0530

    Update main.tf

--- vm/Parent_Module/main.tf
+++ vm/Parent_Module/main.tf
@@ -1,33 +1,33 @@
-# module "rg" {
-#   source = "../child_module/Azurerm_rg"
-#   rgs    = var.rgs1
+module "rg" {
+  source = "../child_module/Azurerm_rg"
+  rgs    = var.rgs1
 
-# }
+ }
 
-# module "vnet" {
-#   depends_on = [module.rg]
-#   source     = "../child_module/Azurerm_vnet"
-#   vnets      = var.vnet1
+module "vnet" {
+  depends_on = [module.rg]
+  source     = "../child_module/Azurerm_vnet"
+  vnets      = var.vnet1
 
-# }
+ }
 
-# module "snet" {
-#   depends_on = [module.vnet]
-#   source     = "../child_module/Azurerm_subnet"
-#   snets      = var.snet1
+module "snet" {
+  depends_on = [module.vnet]
+  source     = "../child_module/Azurerm_subnet"
+  snets      = var.snet1
 
-# }
+ }
 
-# module "pip" {
-#   depends_on = [module.rg]
-#   source     = "../child_module/Azurerm_pip"
-#   pips       = var.pip1
+module "pip" {
+  depends_on = [module.rg]
+  source     = "../child_module/Azurerm_pip"
+  pips       = var.pip1
 
-# }
+ }
 
-# module "nic" {
-#   depends_on = [module.pip, module.rg, module.snet]
-#   source     = "../child_module/Azurerm_nic"
-#   nics       = var.nic1
+module "nic" {
+  depends_on = [module.pip, module.rg, module.snet]
+  source     = "../child_module/Azurerm_nic"
+  nics       = var.nic1
 
-# }
+ }
